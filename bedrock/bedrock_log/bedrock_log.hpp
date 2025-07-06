11238be25e0bc32751697184d3b225a641af3c8e
Reversed Codes <echtedeveloper@gmail.com>
2025-07-06 02:52:48 +0200

    add_public

--- bedrock/bedrock_log/bedrock_log.hpp
+++ bedrock/bedrock_log/bedrock_log.hpp
@@ -14,11 +14,11 @@ namespace BedrockLog
 
     //?_constructAreaFilterFromString@BedrockLog@@YA_NAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@AEAVLogAreaFilter@1@@Z
     TORCHCS_EXPORT bool _constructAreaFilterFromString(
-        const std::string&,
-        LogAreaFilter&);
+        const std::string &,
+        LogAreaFilter &);
 
     //?_constructAreaFilterStringFromFilter@BedrockLog@@YA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@AEBVLogAreaFilter@1@@Z
-    TORCHCS_EXPORT std::string _constructAreaFilterStringFromFilter(const LogAreaFilter&);
+    TORCHCS_EXPORT std::string _constructAreaFilterStringFromFilter(const LogAreaFilter &);
 
     //?_getLog@BedrockLog@@YAAEAULogDetails@1@W4LogCategory@1@W4LogChannel@1@@Z
     TORCHCS_EXPORT LogDetails &_getLog(LogCategory, LogChannel);
@@ -40,12 +40,26 @@ namespace BedrockLog
         BedrockLog::LogCategory,
         BedrockLog::LogChannel,
         bool,
-        LogSettingsUpdater*,
+        LogSettingsUpdater *,
         float);
 
     //?_priorityFilterString@BedrockLog@@YAPEBDI@Z
-    TORCHCS_EXPORT const char* _priorityFilterString(unsigned int);
+    TORCHCS_EXPORT const char *_priorityFilterString(unsigned int);
 
     //?_processIdString@BedrockLog@@YA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@XZ
     TORCHCS_EXPORT std::string _processIdString();
+
+    //?update@BedrockLog@@YAXXZ
+    TORCHCS_EXPORT void update();
+
+    //?updateLogFilter@BedrockLog@@YAXV?$unique_ptr@VLogSettingsUpdater@@U?$default_delete@VLogSettingsUpdater@@@std@@@std@@AEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@3@AEBV?$vector@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V?$allocator@V?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@@2@@3@AEAV43@_N@Z
+    TORCHCS_EXPORT void updateLogFilter(
+        std::unique_ptr<LogSettingsUpdater>,
+        const std::string &,
+        const std::vector<std::string> &,
+        std::vector<std::string> &,
+        bool);
+
+    //?updateLogSetting@BedrockLog@@YAXAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@_N@Z
+    TORCHCS_EXPORT void updateLogSetting(const std::string& name, bool value);
 }
